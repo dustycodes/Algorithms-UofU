ba72e5880f38ef898d0f119089c0ed86bfd4c345
Dusty Argyle <u0770958@utah.edu>
2016-11-15 09:53:23 -0700

    Removing k overhead

--- PS10/narrow-art-gallery.py
+++ PS10/narrow-art-gallery.py
@@ -9,8 +9,6 @@ def max_value(g, k):
 def compute_max_value(g, cache, r, unclosed_room, k):
     if r == len(g):
         return 0
-    #elif (r, k) in cache.keys():
-    #    return cache[r, k]
     else:
         if k == len(g) - r:
             left = 0
@@ -23,21 +21,20 @@ def compute_max_value(g, cache, r, unclosed_room, k):
                 left = g[r][0] + compute_max_value(g, cache, r+1, 0, k-1)
                 right = g[r][1] + compute_max_value(g, cache, r + 1, 1, k - 1)
             m = max(right, left)
-            #cache[r, k] = m
             return m
         else:
             middle = g[r][0] + g[r][1] + compute_max_value(g, cache, r+1, -1, k)
             left = 0
             right = 0
-            if unclosed_room == 0:
-                left = g[r][0] + compute_max_value(g, cache, r+1, 0, k-1)
-            elif unclosed_room == 1:
-                right = g[r][1] + compute_max_value(g, cache, r+1, 1, k-1)
-            else:
-                left = g[r][0] + compute_max_value(g, cache, r+1, 0, k-1)
-                right = g[r][1] + compute_max_value(g, cache, r+1, 1, k-1)
+            if k > 0:
+                if unclosed_room == 0:
+                    left = g[r][0] + compute_max_value(g, cache, r+1, 0, k-1)
+                elif unclosed_room == 1:
+                    right = g[r][1] + compute_max_value(g, cache, r+1, 1, k-1)
+                else:
+                    left = g[r][0] + compute_max_value(g, cache, r+1, 0, k-1)
+                    right = g[r][1] + compute_max_value(g, cache, r+1, 1, k-1)
             m = max(right, left, middle)
-            #cache[r, k] = m
             return m
 
 
